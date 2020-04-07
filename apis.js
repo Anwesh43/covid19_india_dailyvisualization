57c9d8decb4bcd113d996c000160bbb34200d4a1
Anwesh43 <anweshthecool0@gmail.com>
2020-04-08 02:56:13 +0530

    adding api endpoints

---
+++ apis.js
@@ -0,0 +1,7 @@
+const APIS = {
+    DATA: "https://api.covid19india.org/data.json",
+    STATE_DAILY: "https://api.covid19india.org/states_daily.json",
+    TRAVEL_HISTORY: "https://api.covid19india.org/travel_history.json",
+    RAW_DATA: "https://api.covid19india.org/raw_data.json",
+    STATE_DISTRICT_WISE: "https://api.covid19india.org/state_district_wise.json"
+}
